aac07dc6e235d7c8bea083eaf13a023ee7340cad
kevinadhiguna <kevin26798@gmail.com>
2020-12-08 12:20:45 +0700

    refactor: add menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import 'antd/dist/antd.css';
+import React from "react";
+import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
 
-import './App.css';
+import "./App.css";
+
+const { Header } = Layout;
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={["2"]}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+        </Layout>
+    );
 }
 
 export default App;
