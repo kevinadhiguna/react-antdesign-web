e24c40f6941deecd5d8f2c4db599eb003d1e6727
kevinadhiguna <kevin26798@gmail.com>
2020-12-10 18:58:25 +0700

    feat: add About section

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import AppHero from '../components/home/hero';
+import AppAbout from '../components/home/about';
 
 function AppHome() {
-    return(
-        <div className="main">
-            <AppHero />
-        </div>
-    );
+  return (
+    <div className="main">
+      <AppHero />
+      <AppAbout />
+    </div>
+  );
 }
 
 export default AppHome;
